c1b9614e91155e9d201aa2c511bb9af1e7f993c7
Ana Luísa Rogério Pimenta <anapimentainf@gmail.com>
2020-02-18 12:39:20 -0300

    logica basica

---
+++ main.js
@@ -0,0 +1,33 @@
+function novosInputs(qtd){
+    //seleciona area que serão adicionados os inputs 
+    //limpa oq ja tiver la 
+    for (let i = 0; i < qtd; i++) {
+        //area.append input numero 
+
+    }
+    //se botao nao existir, colocar 
+    //adicionar função somaInputs ao click 
+}
+
+function somaInputs(){
+    // seleciona todos os inputs
+    // testa se o campo esta vazio 
+    //percorre somando 
+    //exibe o resultado
+}
+
+function campoVazio(el){
+    //testa se o value é nulo 
+    //lança exceção e mensagem 
+} 
+
+
+
+let $inputQuantidade = $("#inputQuantidade");
+
+let $btnQuantidade = $("btnQuantidade")
+.click(function(){
+
+    campoVazio($inputQuantidade);
+    novosInputs($inputQuantidade.value);
+});
