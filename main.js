99c1cb37c472c02a3d7dac4d3a37d5af50aff307
Ana Luísa Rogério Pimenta <anapimentainf@gmail.com>
2020-02-19 02:49:32 -0300

    pequenas correções

--- main.js
+++ main.js
@@ -1,82 +1,75 @@
 function novosInputs(qtd){
 
-    let $somaContainer = $("#somaContainer").empty(); 
+  //esvazia o a div que contém os inputs da soma
+  let $somaContainer = $("#somaContainer").empty();
 
-
-    for (let i = 0; i < qtd; i++) {
-
-        $somaContainer.append(
-            '<input type="number" class="inputSoma"> '
-        )
-
-    }
+  for (let i = 0; i < qtd; i++) {
 
     $somaContainer.append(
- 
-        '<button id="btnResultado">Resultado</button>'
+      '<input type="number" class="inputSoma"> '
     )
-    
-    $("#btnResultado").click(function(){
+  }
 
-        let $inputs = $(".inputSoma");
-    
-       $inputs.each(function(){
-           campoVazio($(this));
-       });
+  $somaContainer.append(
+    '<button id="btnResultado">Resultado</button>'
+  );
 
-        calculaResultado($inputs);
+  $("#btnResultado").click(function(){
+
+    let $inputs = $(".inputSoma");
+
+    $inputs.each(function(){
+      campoVazio($(this));
     });
+
+    calculaResultado($inputs);
+  });
 }
 
 function calculaResultado(inputs){
-     
-    let total = 0;
 
-    inputs.each(function(){
-        
-       total+= parseInt($(this).val());
-     });
-     
+  let total = 0;
 
-    //Adiciona resultado no modal
-    $("#resultado").html("<p> Resultado da Soma:" + total + "</p>")
-    $("#mostraResultado").click();
+  inputs.each(function(){
+    total+= parseFloat($(this).val());
+  });
+
+  //Adiciona resultado e abre o modal
+  $("#resultado").html("<p> Resultado da Soma = " + total.toFixed(2) + "</p>")
+  $("#mostraResultado").click();
 
 }
 
 function campoVazio(el){
- 
-    //lança exceção caso campo for vazio 
-    if(!el.val()){
+  //lança exceção caso campo for vazio
+  if(!el.val()){
 
-        $("#mostraErro").click();
-        
-        throw "Campo vazio";
-        
-    }
-    
-} 
+    $("#mostraErro").click();
+    throw "Campo vazio";
+
+  }
+}
 
 function campoInvalido(el){
- 
-    //lança exceção caso campo for invalido
-    if(el.val() < 1){
+  //lança exceção caso campo for invalido
+  if(el.val() < 1){
 
-        $("#mostraErro").click();
-        
-        throw "Campo negativo";
-        
-    }
-    
-} 
+    $("#mostraErro").click();
+    throw "Campo negativo";
+  }
+
+  if(!Number.isInteger(parseFloat(el.val()))){
+
+    $("#mostraErro").click();
+    throw "Campo não inteiro";
+  }
+}
 
 let $inputQuantidade = $("#inputQuantidade");
 
 let $btnQuantidade = $("#btnQuantidade")
 .click(function(){
-
-    campoVazio($inputQuantidade);
-    campoInvalido($inputQuantidade);
-    //talvez colocar valor max 
-    novosInputs($inputQuantidade.val());
+  campoVazio($inputQuantidade);
+  campoInvalido($inputQuantidade);
+  novosInputs($inputQuantidade.val());
 });
